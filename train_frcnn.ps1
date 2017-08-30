0d95a11c6af9f315f27fb66c5f38034af3806549
Alexander Pacha <alexander.pacha@gmail.com>
2017-08-30 17:46:21 +0200

    Adding PS-Script that trains and tracks the process

---
+++ train_frcnn.ps1
@@ -0,0 +1,19 @@
+﻿$pathToSourceRoot = "C:\Users\Alex\Repositories\keras-frcnn\"
+$pathToTranscript = "$($pathToSourceRoot)"
+
+# Allowing wider outputs https://stackoverflow.com/questions/7158142/prevent-powergui-from-truncating-the-output
+$pshost = get-host
+$pswindow = $pshost.ui.rawui
+$newsize = $pswindow.buffersize
+$newsize.height = 9999
+$newsize.width = 1500
+$pswindow.buffersize = $newsize
+
+cd $pathToSourceRoot
+echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
+$env:PYTHONPATH = $pathToSourceRoot
+
+# Started on Donkey, 17.08.2017
+Start-Transcript -path "$($pathToTranscript)2017-08-18_train_frcnn.txt" -append
+python "$($pathToSourceRoot)train_frcnn.py" --path "$($pathToSourceRoot)VOC_Data"
+Stop-Transcript
